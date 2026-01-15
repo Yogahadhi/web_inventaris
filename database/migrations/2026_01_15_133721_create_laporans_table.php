933cb9eecee5e9144b1f816921b0aa9833f53e33
Yogahadhi <yogazen137@gmail.com>
2026-01-15 14:00:59 +0800

    update test data laporan

---
+++ database/migrations/2026_01_15_133721_create_laporans_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('laporans', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_perangkat');
+            $table->string('nama');
+            $table->enum('kategori', ['Komputer', 'Mouse', 'Monitor', 'Keyboard']);
+            $table->string('merek');
+            $table->string('model');
+            $table->enum('kondisi', ['Layak_Pakai', 'Perlu_Perbaikan', 'Rusak']);
+            $table->string('lokasi');
+            $table->string('tanggal');
+            $table->foreignId('operator')
+                    ->constrained('user')
+                    ->onDelete('restrict');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('laporans');
+    }
+};
